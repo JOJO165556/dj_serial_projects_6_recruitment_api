179c40d92fc908a25d8a5043748c72f72f86b522
JOJO <hounkpatijosue2020@gmail.com>
2026-04-05 23:57:13 +0000

    feat: Ajout de auth JWT

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class MeView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
--- config/settings/base.py
+++ config/settings/base.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'apps.accounts',
     'apps.analytics',
     'apps.applications',
