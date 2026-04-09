15251217f1cdd0b58b1c81c6687bdf43357b4e3d
JOJO <hounkpatijosue2020@gmail.com>
2026-04-09 00:01:20 +0000

    feat(analytics): création dashboard stats et refactorisation DRY des requêtes base de données

---
+++ apps/analytics/services.py
@@ -0,0 +1,59 @@
+from apps.jobs.models import JobOffer
+from apps.applications.models import Application
+from apps.jobs.models import JobView
+
+
+def recruiter_stats(user):
+    """
+    Statistiques globales et analytics avancées pour un recruteur.
+    """
+    jobs = JobOffer.objects.filter(company__owner=user)
+    applications = Application.objects.filter(job_offer__in=jobs)
+    views = JobView.objects.filter(job__in=jobs)
+
+    total_jobs = jobs.count()
+    total_applications = applications.count()
+    total_views = views.count()
+
+    #Evite division par 0
+    conversion_rate = (
+        (total_applications / total_views) * 100
+        if total_views > 0 else 0
+    )
+
+    #Job le plus performant
+    best_job = None
+    best_count = 0
+
+    for job in jobs:
+        count = job.applications.count() # type: ignore
+        if count > best_count:
+            best_count = count
+            best_job = job.title
+
+    return {
+        "total_jobs": total_jobs,
+        "total_views": total_views,
+        "total_applications": total_applications,
+        "interview": applications.filter(status="INTERVIEW").count(),
+        "accepted": applications.filter(status="ACCEPTED").count(),
+        "rejected": applications.filter(status="REJECTED").count(),
+        "conversion_rate": round(conversion_rate, 2),
+        "best_job": best_job,
+    }
+
+def candidate_stats(user):
+    """
+    Statistiques pour un candidat.
+    """
+
+    applications = Application.objects.filter(candidate=user)
+
+    return {
+        "total_applications": applications.count(),
+        "accepted": applications.filter(status="ACCEPTED").count(),
+        "rejected": applications.filter(status="REJECTED").count(),
+        "in_progress": applications.exclude(
+            status__in=["ACCEPTED", "REJECTED"]
+        ).count(),
+    }
--- apps/analytics/views.py
+++ apps/analytics/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .services import recruiter_stats, candidate_stats
+
+class AnalyticsView(APIView):
+    """
+    Retourne les stats selon le rôle de user.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        if user.role == "RECRUITER":
+            data = recruiter_stats(user)
+
+        elif user.role == "CANDIDATE":
+            data = candidate_stats(user)
+
+        else:
+            data = {}
+
+        return Response(data)
